622f74f034480b955e1591fae22ddf71a00a12d5
shyammoulika <moulika.cheekati@gmail.com>
2023-09-24 10:54:07 +0530

    navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { authGuard } from './auth.guard';
+import { LandingComponent } from './public/landing/landing.component';
 
 const routes: Routes = [
+
+  // {path:'**',component:LandingComponent},
   {path: '',loadChildren:()=>import('./public/public.module').then((m)=>m.PublicModule)},
   {path: 'admin',loadChildren:()=>import('./admin/admin.module').then((m)=>m.AdminModule),
 
@@ -12,7 +15,8 @@ const routes: Routes = [
   {
     path:'login',
   component:LoginComponent,
-  }
+  },
+
 ];
 
 @NgModule({
